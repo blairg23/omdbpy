21b2eb65b3b4820dd2e7ef31f4832a6f6220ffe9
blairg23 <blairg23@gmail.com>
2019-08-16 00:56:39 -0600

    Refactored unit tests and testing directory structure. * Moved tests into source folder for better modularity and separation of concerns. Also easier importing. * Refactored tests to mock the `requests` object instead of the `Api()` object, as we want to use properties of `Api()` to assert equality in the unit tests and we don't want to actually generate HTTP requests when testing.

--- src/omdb/omdb.py
+++ src/omdb/omdb.py
@@ -3,7 +3,6 @@ A minimalist Python wrapper for the Open Movie Database (OMDb) API (https://www.
 """
 
 import requests
-import json
 
 
 class Api:
@@ -18,10 +17,10 @@ class Api:
         self._api_version = api_version
         self._url = 'https://www.omdbapi.com/'
 
-    def search(self, search_string=None, imdb_id=None, title=None, result_type=None, release_year=None, plot='full', return_type='json', page=None, callback=None, season=None, episode=None):
+    def search(self, search_terms=None, imdb_id=None, title=None, result_type=None, release_year=None, plot='full', return_type='json', page=None, callback=None, season=None, episode=None):
         """
 
-        :param str search_string: Any search phrase that might identify a possible movie title. [optional]
+        :param str search_terms: Any search phrase that might identify a possible movie title. [optional]
         :param str imdb_id: A valid IMDb ID (e.g. tt1285016). [optional]
         :param str title: Movie title to search for. [optional]
         :param str result_type: Type of result to return. [optional]. Valid Options: [`movie`, `series`, `episode`]
@@ -38,7 +37,7 @@ class Api:
 
         payload = {
             'apikey': self._apikey,
-            's': search_string,
+            's': search_terms,
             'i': imdb_id,
             't': title,
             'type': result_type,
@@ -54,11 +53,3 @@ class Api:
         response = requests.get(url=self._url, params=payload)
 
         return response
-
-
-# if __name__ == '__main__':
-#     # Get your free API key at https://www.omdbapi.com/apikey.aspx
-#     # You can use mine, but you'll be sharing it with other users of this library!
-#     omdb_api = Api(apikey='7b7c7bc4')
-#     result = omdb_api.search(search_string='terminator', release_year='1984')
-#     print(json.dumps(result.json(), indent=4))
---
+++ src/tests/test_omdb.py
@@ -0,0 +1,38 @@
+from unittest import mock, TestCase
+
+import faker
+import omdb
+
+module_under_test = 'omdb'
+
+fake = faker.Faker()
+
+
+class ApiTestCase(TestCase):
+    def setUp(self):
+        self.mock_requests_patch = mock.patch(f'{module_under_test}.omdb.requests', autospec=True)
+        self.mock_requests = self.mock_requests_patch.start()
+
+        self._apikey = fake.word
+        self._api_version = str(fake.pyint)
+        self._omdb_api = omdb.Api(apikey=self._apikey, api_version=self._api_version)
+
+    def tearDown(self):
+        self.mock_requests_patch.stop()
+
+    def test_initialization(self):
+        self.assertIsInstance(self._omdb_api, omdb.Api)
+        self.assertEqual(self._omdb_api._apikey, self._apikey)
+        self.assertEqual(self._omdb_api._api_version, self._api_version)
+
+    def test_search_by_search_terms(self):
+        self._omdb_api.search(search_terms=fake.words, release_year=fake.year)
+        self.mock_requests.get.assert_called_once()
+
+    def test_search_by_title(self):
+        self._omdb_api.search(title=fake.words, release_year=fake.year)
+        self.mock_requests.get.assert_called_once()
+
+    def test_search_by_imdb_id(self):
+        self._omdb_api.search(imdb_id=fake.words, release_year=fake.year)
+        self.mock_requests.get.assert_called_once()
